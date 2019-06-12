46649287ad6c0f6b8ec1c874238e227215b70962
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-12 07:26:36 +0000

    Deploy NewDadaFE/react-impression to github.com/NewDadaFE/react-impression.git:gh-pages

---
+++ examples/precache-manifest.361816d71c99adcf542edc12ed401619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2679328dfe0e6a1ba336",
+    "url": "./static/js/main.2679328d.chunk.js"
+  },
+  {
+    "revision": "399684c737c059f01bbe",
+    "url": "./static/js/1.399684c7.chunk.js"
+  },
+  {
+    "revision": "2679328dfe0e6a1ba336",
+    "url": "./static/css/main.1db88132.chunk.css"
+  },
+  {
+    "revision": "399684c737c059f01bbe",
+    "url": "./static/css/1.29ee98d3.chunk.css"
+  },
+  {
+    "revision": "92093ea14026e0bfdc5eb235e22d759a",
+    "url": "./index.html"
+  }
+];
